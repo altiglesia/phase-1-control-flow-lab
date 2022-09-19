95c1c0df29f6587c3bbb9a27fe9effa0a3b431c4
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-18 20:40:13 -0400

    commit tree 9ccd0a3bacb66fcde0684e4d57fac8b63a240d5b

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let ride
+  if (someValue <= 400){
+    ride = "This one is on me!"
+    }
+  else if (400 < someValue && someValue < 2000){
+    ride = "That will be twenty bucks."
+  }
+  else if (2000 < someValue && someValue < 2500){
+    ride = "I will gladly take your thirty bucks."
+  }
+  else if (someValue > 2500){
+    ride = "No can do."
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  switch(charm){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
